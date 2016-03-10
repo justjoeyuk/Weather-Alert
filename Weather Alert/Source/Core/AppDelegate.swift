33c057c80443e4a286766687f7d5143d3f0cab4d
Joey Clover <joey.clover@theladbiblegroup.com>
2016-03-10 21:01:34 +0000

    Initial Commit

---
+++ Weather Alert/Source/Core/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//
+
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        setupInitialViewController()
+        return true
+    }
+    
+    private func setupInitialViewController() {
+        let appFrame = UIScreen.mainScreen().bounds
+        window = UIWindow(frame: appFrame)
+        
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+    }
+    
+}
